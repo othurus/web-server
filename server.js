b2666dcff65092931f72ead4c0a8ebf9c7d6f186
Faisal <mr.fiesal@gmail.com>
2016-11-12 06:07:53 +0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+//app.get('/', function(req, res){
+//    res.send('Hello express!');
+//});
+
+var middleware = {
+    requireAuthentication: function(req, res, next){
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log('Request Date: ' + new Date().toString());
+        console.log('Request method: ' + req.method);
+        console.log('Original URL: ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication , function(req, res){
+   res.send('About us!') ;
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//console.log(__dirname);
+
+app.listen(port, function(){
+    console.log('Express server is listening at port ' + port + '!'); 
+});
+
+ 
